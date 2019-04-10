85f0493ee77fdbff18ed4ab64038e1c05eec9331
Martín Acosta <tinchoz49@gmail.com>
2019-04-10 15:44:29 -0300

    Added SignalSwarmServer class.

--- server.js
+++ server.js
@@ -1,70 +1,79 @@
 const debug = require('debug')('discovery-swarm-webrtc')
+const SignalServer = require('./lib/signal-server')
 
-module.exports = function createServer ({ io }) {
-  const signalServer = require('./lib/signal-server')(io)
+class SignalSwarmServer extends SignalServer {
+  constructor ({ io }) {
+    super(io)
 
-  const channels = new Map()
+    this.channels = new Map()
 
-  signalServer.on('discover', (request) => {
-    const { id } = request.socket
-    const { id: peerId, channel: channelName } = request.discoveryData
+    this._initialize()
+  }
 
-    let channel = new Map()
+  _initialize () {
+    this.on('discover', (request) => {
+      const { id } = request.socket
+      const { id: peerId, channel: channelName } = request.discoveryData
 
-    if (channels.has(channelName)) {
-      channel = channels.get(channelName)
-    } else {
-      channels.set(channelName, channel)
-    }
+      let channel = new Map()
 
-    channel.set(id, peerId)
+      if (this.channels.has(channelName)) {
+        channel = this.channels.get(channelName)
+      } else {
+        this.channels.set(channelName, channel)
+      }
 
-    debug(`discover: ${peerId} channel: ${channelName}`)
+      channel.set(id, peerId)
 
-    request.discover(peerId, { peers: Array.from(channel.values()), channel: channelName })
-  })
+      debug(`discover: ${peerId} channel: ${channelName}`)
 
-  signalServer.on('disconnect', (socket) => {
-    const { id } = socket
-    channels.forEach((channel, channelName) => {
+      request.discover(peerId, { peers: Array.from(channel.values()), channel: channelName })
+    })
+
+    this.on('disconnect', (socket) => {
+      const { id } = socket
+      this.channels.forEach((channel, channelName) => {
+        const peerId = channel.get(id)
+        if (peerId && channel.delete(id)) {
+          debug(`disconnect: ${peerId} channel: ${channelName}`)
+        }
+      })
+    })
+
+    this.on('request', (request) => {
+      debug(request.initiator, ' -> ', request.target)
+      request.forward() // forward all requests to connect
+    })
+
+    this.on('candidates', request => {
+      const { channel: channelName } = request.discoveryData
+
+      const channel = this.channels.get(channelName) || new Map()
+
+      return request.forward({ peers: Array.from(channel.values()), channel: channelName })
+    })
+
+    this.on('leave', request => {
+      const { id } = request.socket
+
+      const { channel: channelName } = request.discoveryData
+
+      if (!this.channels.has(channelName)) {
+        return request.forward()
+      }
+
+      const channel = this.channels.get(channelName)
+
       const peerId = channel.get(id)
       if (peerId && channel.delete(id)) {
-        debug(`disconnect: ${peerId} channel: ${channelName}`)
+        debug(`leave: ${peerId} channel: ${channelName}`)
       }
-    })
-  })
 
-  signalServer.on('request', (request) => {
-    debug(request.initiator, ' -> ', request.target)
-    request.forward() // forward all requests to connect
-  })
-
-  signalServer.on('candidates', request => {
-    const { channel: channelName } = request.discoveryData
-
-    const channel = channels.get(channelName) || new Map()
-
-    return request.forward({ peers: Array.from(channel.values()), channel: channelName })
-  })
-
-  signalServer.on('leave', request => {
-    const { id } = request.socket
-
-    const { channel: channelName } = request.discoveryData
-
-    if (!channels.has(channelName)) {
       return request.forward()
-    }
-
-    const channel = channels.get(channelName)
-
-    const peerId = channel.get(id)
-    if (peerId && channel.delete(id)) {
-      debug(`leave: ${peerId} channel: ${channelName}`)
-    }
-
-    return request.forward()
-  })
-
-  return { signalServer, channels }
+    })
+  }
 }
+
+module.exports = (...args) => new SignalSwarmServer(...args)
+
+module.exports.SignalSwarmServer = SignalSwarmServer
