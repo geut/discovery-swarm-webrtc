12661f084d68b6b31d1ecaa61733b8ed51788b1a
Martin Acosta <tinchoz49@gmail.com>
2019-09-16 09:46:50 -0300

    Improve connectivity test.

--- tests/connectivity.test.js
+++ tests/connectivity.test.js
@@ -12,7 +12,6 @@ const log = debug('test:connectivity')
 debug.enable('test:connectivity')
 
 const MAX_NODES = 50
-const MIN_LINKS = 56
 const TIMEOUT = 30 * 1000
 
 jest.setTimeout(TIMEOUT)
@@ -48,7 +47,7 @@ afterEach(async () => {
   return Promise.all(wait)
 })
 
-test(`graph connectivity for ${MAX_NODES} peers (minConnections=${MIN_LINKS})`, async () => {
+test(`graph connectivity for ${MAX_NODES} peers.`, async () => {
   const swarms = [...Array(MAX_NODES).keys()].map(n => addPeer(
     this.graph,
     this.topic,
@@ -60,19 +59,15 @@ test(`graph connectivity for ${MAX_NODES} peers (minConnections=${MIN_LINKS})`, 
     }
   ))
 
-  log(`Waiting for a minimum amount of ${MIN_LINKS} connections.`)
-
-  await waitForExpect(() => {
-    expect(this.graph.getLinksCount()).toBeGreaterThanOrEqual(MIN_LINKS)
-  }, Math.floor(TIMEOUT / 2), 1 * 1000)
-
   log(`Testing connectivity for ${this.graph.getNodesCount()} peers.`)
 
-  const pathFinder = createGraphPath.aStar(this.graph)
-  const fromId = swarms[0].id.toString('hex')
-  this.graph.forEachNode(function (node) {
-    if (node.id === fromId) return
-    expect(pathFinder.find(fromId, node.id).length).toBeGreaterThan(0)
-    expect(node.data.getPeers().find(peer => !peer.socket)).toBeUndefined()
-  })
+  await waitForExpect(() => {
+    const pathFinder = createGraphPath.aStar(this.graph)
+    const fromId = swarms[0].id.toString('hex')
+    this.graph.forEachNode(function (node) {
+      if (node.id === fromId) return
+      expect(pathFinder.find(fromId, node.id).length).toBeGreaterThan(0)
+      expect(node.data.getPeers().find(peer => !peer.socket)).toBeUndefined()
+    })
+  }, TIMEOUT, 10 * 1000)
 })
