5bf2a0a2e89fd88df528ceab05ac4416d0421c24
Martin Acosta <tinchoz49@gmail.com>
2020-04-07 11:44:28 -0300

    Added server file for backward compatibility

---
+++ server.js
@@ -0,0 +1,5 @@
+const { SocketSignalWebsocketServer } = require('socket-signal-websocket')
+
+// Backward compatibility with v2
+module.exports = (...args) => new SocketSignalWebsocketServer(...args)
+module.exports.SignalSwarmServer = SocketSignalWebsocketServer
