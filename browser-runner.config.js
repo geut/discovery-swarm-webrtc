6ab4f29280c77a7b438ce42957f90e66810686e2
Martin Acosta <tinchoz49@gmail.com>
2020-05-26 16:35:08 -0300

    Added dxos/browser-runner for browser testing

--- browser-runner.config.js
+++ browser-runner.config.js
@@ -6,24 +6,28 @@ let server = null
 let stream = null
 
 module.exports = {
-  async beforeAll ({ shutdown }) {
+  async beforeAll () {
     server = createServer()
 
     const signal = new SocketSignalWebsocketServer({ server, requestTimeout: 10 * 1000 })
     signal.on('error', err => console.log('signal', err.message))
-
-    stream = finished(function (results) {
-      if (results.ok) {
-        return shutdown(0)
-      }
-      shutdown(1)
-    })
 
     return new Promise(resolve => server.listen(3001, () => resolve()))
   },
   afterAll () {
     if (!server) return
     server.close()
+  },
+  onExecute ({ options: { watch }, shutdown }) {
+    if (stream) stream.end()
+    stream = finished(function (results) {
+      if (watch) return
+
+      if (results.ok) {
+        return shutdown(0)
+      }
+      shutdown(1)
+    })
   },
   onMessage (msg) {
     stream.write(msg + '\n')
